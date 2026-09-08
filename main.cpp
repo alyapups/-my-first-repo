3aa5e2c1051bfff3364c3eb5e30b17521fa9772d
Алина <143899562+alyapups@users.noreply.github.com>
2026-09-08 21:26:55 +0300

    хлеб

--- main.cpp
+++ main.cpp
@@ -1,26 +1,72 @@
-//Вычислить сумму первых
-//N
-// нечётных чисел. Количество чисел
-//N
-// вводится с клавиатуры.
 #include <iostream>
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 int main() {
     using std::cout;
     using std::cin;
-    using std::endl;
-    int N;
-    cout << "enter N ";
-    cin>>N;
-    //int sum=((2+(N-1)*2)*N)/2;
-    int sum=N*N;
-    cout << sum << endl;
+    int n, k, t, l;
+    int b = 0;
+    cout << "enter primary sum, price for bread, times of going before going for money b4 visiting grandma, price for ice cream";
+    cin >> n >> k >> t >> l;
+    int tt = t;
+    if (n == 0)
+        cout << "grandma gets flowers and that s it";
+
+    else if (n >= k) {
+    cout << "bread is bought the amount of money left is " << n - k;
+    b=1;
+    }
+    else {
+        if (t == 0) {
+            if (n > l) {
+                n -= l;
+                cout << "grandma gets ice cream and boy goes back to the track";
+            }
+            else if (n == l) {
+                cout << "grandma gets ice cream and it ll never be enough for bread";
+            }
+            else {
+                cout << "grandma gets flowers and boy goes back to the track";
+            }
+        }
+        else if (t > 0) {
+            int zero = 0;
+            if (tt % 2 == 0) {
+                tt += 1;
+                zero += 1;
+            }
+            while (tt > zero && n < k) {
+                n *= ((tt % 2) + 2);
+                tt--;
+            }
+            if (n >= k) {
+                b = 1;
+                cout << "bread is bought, money left - " << n - k;
+            }
+            else if (tt == zero) {
+                if ((n > l)) {
+                    n -= l;
+                    cout << "grandma gets ice cream and boy goes back to the track";
+                } else if ((n == l)) {
+                    n -= l;
+                    cout << "grandma gets ice cream and it ll never be enough for bread";
+                } else {
+                    cout << "grandma gets flowers and boy goes back";
+                }
+            }
+        }
+        if (b == 0 && n != 0) {
+            if ((t%2==0))
+                tt = 2147483647;
+            else
+                tt = 2147483646;
+            while (n < k) {
+                n *= ((tt % 2) + 2);
+                tt--;
+            }
+            cout << "THE AMOUNT LEFT - " << n - k;
+        }
+    }
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
