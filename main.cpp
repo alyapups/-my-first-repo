ce92232df1041937481881e3a04d6303c8882139
Алина <143899562+alyapups@users.noreply.github.com>
2026-09-08 21:25:55 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,34 +1,20 @@
-
-//Даны два натуральных числа
-//n
-//,
-//m
-// (вводятся с клавиатуры). Вывести все их общие делители.
+//Вычислить сумму первых
+//N
+// нечётных чисел. Количество чисел
+//N
+// вводится с клавиатуры.
 #include <iostream>
-#include <cmath>
-
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     using std::cout;
     using std::cin;
     using std::endl;
-    int a,b;
-    cout << "enter two numbers";
-    cin >> a >> b;
-    //находим нод методом евклида
-    while(a!=0 && b!=0){
-        if(a>=b)
-            a=a%b;
-        else
-            b=b%a;
-    }
-    int sum=a+b;
-    int sq=sqrt(sum);
-    for(int i =1; i <= sq; i++){
-        if(sum%i==0)
-            cout << i << " ";
-    }
+    int N;
+    cout << "enter N ";
+    cin>>N;
+    //int sum=((2+(N-1)*2)*N)/2;
+    int sum=N*N;
     cout << sum << endl;
 
     return 0;
