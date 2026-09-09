4455a73fb285c2ca5d94b0a3e24ff2d058febe0b
Алина <143899562+alyapups@users.noreply.github.com>
2026-09-09 14:02:02 +0300

    Update and rename 3.cpp to T1_3.cpp

--- T1_3.cpp
+++ T1_3.cpp
@@ -1,8 +1,7 @@
 //С клавиатуры вводится четырёхзначное натуральное число. Выяснить, является ли оно палиндромом
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 int main() {
     using std::cout;
     using std::cin;
@@ -22,8 +21,3 @@ int main() {
         cout << "no" << endl;
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
