9a69f09f7fe4509a268b205c440cea14d1dff343
Алина <143899562+alyapups@users.noreply.github.com>
2026-09-09 14:01:17 +0300

    Update and rename 2.cpp to T1_2.cpp

---
+++ T1_2.cpp
@@ -0,0 +1,19 @@
+//Вычислить сумму первых
+//N
+// нечётных чисел. Количество чисел
+//N
+// вводится с клавиатуры.
+#include <iostream>
+
+int main() {
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    int N;
+    cout << "enter N ";
+    cin>>N;
+    int sum=N*N;
+    cout << sum << endl;
+
+    return 0;
+}
