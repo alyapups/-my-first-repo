d3681b97ae1c05bd7ddfdfcf2869cbf80f59bc1a
Алина <143899562+alyapups@users.noreply.github.com>
2026-09-15 17:19:21 +0300

    +проверка для д

--- T1_6.cpp
+++ T1_6.cpp
@@ -7,6 +7,10 @@ int main()
 {
     int a, b, d;
     cin >> a >> b >> d;
+    if(d==0){
+        cout << "error";
+        return 1;
+    }
     int n = (b - a)/d + 1;
     for (int i = 1; i <= n; i++) {
         int an = (a + (i - 1) * d);
