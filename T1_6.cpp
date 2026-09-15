7be2dbe9db99e055dfac291da1ed345a83893394
Ilya Shablinsky <74829307+ishablinskij@users.noreply.github.com>
2026-09-15 15:51:19 +0300

    Оптимизировал алгоритм

--- T1_6.cpp
+++ T1_6.cpp
@@ -1,34 +1,17 @@
-/*Дана арифметическая прогрессия: a k = a k − 1 + d .
- * Вывести все члены этой прогрессии, кратные 3, которые лежат на отрезке [ a , b ] .
- * Числа a , b , d вводятся с клавиатуры( a 0 = a ).*/
 #include <iostream>
 
+using namespace std;
 
-int main() {
-    using std::cout;
-    using std::cin;
-    using std::endl;
+/*Дана арифметическая прогрессия. Вывести все члены этой прогрессии, кратные 3, которое лежат на отрезке*/
+int main()
+{
     int a, b, d;
-    cout << " enter a b d ([a,b]) " << endl;
     cin >> a >> b >> d;
-    if (d == 0) {
-        if (a % 3 == 0)
-            cout << a;
-    }
-    else if (d > 0) {
-        int ak = a;
-        while (ak <= b) {
-            if (ak % 3 == 0)
-                cout << ak << endl;
-            ak += d;
-        }
-    }
-    else if(d<0){
-        int ak = a;
-        while (ak >= b) {
-            if (ak % 3 == 0)
-                cout << ak << endl;
-            ak += d;
+    int n = (b - a)/d + 1;
+    for (int i = 1; i <= n; i++) {
+        int an = (a + (i - 1) * d);
+        if (an % 3 == 0) {
+            cout << an << " ";
         }
     }
     return 0;
