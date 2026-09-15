293e39e9d6a2dc4c39208a971d9c3f41e272cdea
Алина <143899562+alyapups@users.noreply.github.com>
2026-09-15 17:14:50 +0300

    +проверка -лиший вывод 0

--- T1_5.cpp
+++ T1_5.cpp
@@ -5,13 +5,18 @@ using namespace std;
 int main()
 {
     int n = 0;
+    cout << "enter n";
     cin >> n;
+    if(n<0){
+        cout << "error";
+        return 1;
+    }
 
     int pr = 0;
     int next = 1;
     int x;
-    cout << pr << " ";
-    for (int i = 0; i < n; i++) {
+   cout << pr << " ";
+    for (int i = 1; i < n; i++) {
         cout << next << " ";
         x = pr;
         pr = next;
